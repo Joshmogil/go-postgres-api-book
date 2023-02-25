c13e29394b3df2018f4b9eb0c08eed7cfe487208
Josh Mogil <jmogil@pop-os.localdomain>
2023-02-25 10:35:42 -0500

    validator

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -6,33 +6,61 @@ import (
 	"time"
 
 	"github.com/Joshmogil/go-postgres-api-book/internal/data"
-
-
+	"github.com/Joshmogil/go-postgres-api-book/internal/validator"
 )
 
-func (app *application) createMovieHandler( w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"` // Make this field a data.Runtime type.
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator.
+	v := validator.New()
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w,r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
 	movie := data.Movie{
-		ID: 	id,
+		ID:        id,
 		CreatedAt: time.Now(),
-		Title: "Casablanca",
-		Runtime:  102,
-		Genres: []string{"drama", "romance", "war"},
-		Version: 1,
-	}	
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
 
-	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
